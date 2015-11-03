5c180b359e08c85c37be4127dbc0f9258938ca7f
luluwuluying <443726224@qq.com>
2015-11-02 19:23:45 -0500

    First commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,7 @@ module.exports = function (grunt) {
                 options: {
                     watchTask: true,
                     // fill in proxy address of local WP server
-                    proxy: ""
+                    proxy: "http://luying.com:8888/"
                 }
             }
         }
